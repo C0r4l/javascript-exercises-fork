f85a76849622f9eba7f65c67b85fe7e9718054e0
Panos <panosoiko00@gmail.com>
2023-12-20 13:13:21 +0200

    Finish the exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+  let result = "";
+  if (num < 0) {
+    result += "ERROR";
+  } else {
+    for (let i = 0; i < num; i++) {
+      if (num < 0) result = "ERROR";
+      result += string;
+    }
+  }
+  return result;
 };
 
 // Do not edit below this line
--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,10 @@
-const leapYears = function() {
+const leapYears = function(a) {
+      if (a % 100 === 0) {
+            if (a % 400 === 0) return true;
+      } else if (a % 4 === 0) return true;
 
+      return false;
+      
 };
 
 // Do not edit below this line
